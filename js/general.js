6b1e16e090777faab4871e1d10d737f0a6e809cb
GspeyanHov <gspeyanhov@gmail.com>
2023-02-14 16:56:51 +0400

    added fetch function for crud

--- js/general.js
+++ js/general.js
@@ -1,46 +1,105 @@
-/**
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
- */
-const todos = [];
+let todos = {items: [], filter: 'all'};
 
 const container = select('#todos-container');
 const newTodoInput = select('#new-todo');
 const filterSelect = select("#filter-todos");
+const todoDescription = select("#todo-description");
+const searchTodoById = select("#search");
 
-newTodoInput.addEventListener('keydown', e => {
-    if (e.key === 'Enter') {
-        const text = newTodoInput.value || prompt("Please type the title of the task!");
-
-        if(!text) return;
-
-        todos.push({text, completed: false});
-
-        newTodoInput.value = '';
-
-        reRenderTodoListItems();
-    }
-});
-
-filterSelect.addEventListener("change", function (e) {
+filterSelect.addEventListener("change", function () {
     let selectedFilter = filterSelect.value;
-
     const filterBy = {
-        all: reRenderTodoListItems,
+        all: () => reRenderTodoListItems(todos),
         completed: () => reRenderTodoListItems(todos.filter(todo => todo.completed)),
         uncompleted: () => reRenderTodoListItems(todos.filter(todo => !todo.completed)),
     };
-
     filterBy[selectedFilter]?.();
+    reRenderTodoListItems(todos.items.filter(todos));
+
+
 });
+
+newTodoInput.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        const title = newTodoInput.value || prompt("Please type the title of the task!");
+        const description = todoDescription.value || prompt("Please type the description of the task!")
+
+        if (!title) return;
+
+        fetch('http://localhost:3000/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                completed: false
+            })
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(todo => {
+                console.log(todo);
+                todos.push(todo);
+                if (filterSelect.value === "completed" || filterSelect.value === "uncompleted" || filterSelect.value === "") {
+                    filterSelect.value = "all";
+                }
+                reRenderTodoListItems(todos);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+});
+document.addEventListener('DOMContentLoaded', async function () {
+    todos = await getTodos();
+    reRenderTodoListItems(todos);
+});
+
+async function getTodos() {
+    const response = await fetch('http://localhost:3000/');
+    return await response.json();
+}
 
 function reRenderTodoListItems(todos) {
     container.innerHTML = '';
-    renderTodoListItems(todos);
+    renderTodoListItems(todos.filter(todo => !todo.deleted));
 }
+
+searchTodoById.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        const todoId = e.target.value.trim();
+        if (todoId !== '') {
+            fetch(`http://localhost:3000/` + todoId, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then(todo => {
+                    console.log(todo);
+                    if (filterSelect.value === "completed" || filterSelect.value === "uncompleted" || filterSelect.value === 'all') {
+                        filterSelect.value = '';
+                    }
+                    reRenderTodoListItems([todo]);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    }
+});
 
 function renderTodoListItems(todoList = todos) {
     todoList = todoList.filter(todo => !todo.deleted);
@@ -48,17 +107,58 @@ function renderTodoListItems(todoList = todos) {
         const itemContainer = createElement('li');
 
         /* Task title */
-        const itemText = createElement('div');
-        itemText.style.cursor = 'pointer';
-        itemText.innerHTML = todo.text;
-        itemText.onclick = () => {
+        const id = createElement('div');
+        const title = createElement('div');
+        const description = createElement('div');
+        const completed = createElement('div');
+        const createdAt = createElement('div');
+        const updatedAt = createElement('div');
+
+        title.style.cursor = 'pointer';
+        id.innerHTML = todo.id;
+        title.innerHTML = todo.title;
+        description.innerHTML = todo.description;
+        completed.innerHTML = todo.completed;
+        createdAt.innerHTML = todo.createdAt;
+        updatedAt.innerHTML = todo.updatedAt;
+
+        title.onclick = () => {
             todo.completed = !todo.completed;
+            fetch('http://localhost:3000/todos/' + todo.id, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title: todo.title,
+                    description: todo.description,
+                    completed: todo.completed
+                })
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then(updatedTodo => {
+                    console.log(updatedTodo);
+
+                })
+                .catch(error => {
+                    console.error(error);
+                });
             reRenderTodoListItems(todoList);
         }
         if (todo.completed) {
-            itemText.style['text-decoration'] = 'line-through';
+            title.style['text-decoration'] = 'line-through';
         }
-        itemContainer.appendChild(itemText);
+        itemContainer.appendChild(id);
+        itemContainer.appendChild(title);
+        itemContainer.appendChild(description);
+        itemContainer.appendChild(completed);
+        itemContainer.appendChild(createdAt);
+        itemContainer.appendChild(updatedAt);
 
 
         /* Delete button */
@@ -68,6 +168,31 @@ function renderTodoListItems(todoList = todos) {
             const currentTodoIndex = todoList.findIndex(t => t === todo);
             todoList.splice(currentTodoIndex, 1);
             todo.deleted = true;
+            fetch('http://localhost:3000/todos/' + todo.id, {
+                method: 'Delete',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title: todo.title,
+                    description: todo.description,
+                    completed: todo.completed
+                })
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then(updatedTodo => {
+                    console.log(updatedTodo);
+
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+
             reRenderTodoListItems(todoList);
         }
         itemContainer.appendChild(itemDelete);
@@ -77,10 +202,10 @@ function renderTodoListItems(todoList = todos) {
     });
 }
 
-function select(){
+function select() {
     return document.querySelector(...arguments)
 }
 
-function createElement(){
+function createElement() {
     return document.createElement(...arguments)
 }
